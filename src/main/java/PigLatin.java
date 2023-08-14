ce18493b61a59239ede007a2267ca027dcf201ba
Trathen231 M <trathen231@gmail.com>
2023-08-14 16:19:30 +0000

    Java PigLatin

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -16,7 +16,17 @@ public class PigLatin {
      * @param in A string.
      * @return the pig latin form of in.
      */
-    public String returnPigLatin(String in){
-        return "";
+    public String returnPigLatin(String in) {
+        if (in == null || in.isEmpty()) {
+            return ""; // Return an empty string for null or empty input
+        }
+        
+        char firstChar = in.charAt(0);
+        String restOfWord = in.substring(1); // Get the substring from index 1 to the end
+        
+        // Construct the Pig Latin word by moving the first character to the end and adding "ay"
+        String pigLatinWord = restOfWord + firstChar + "ay";
+        
+        return pigLatinWord;
     }
 }
